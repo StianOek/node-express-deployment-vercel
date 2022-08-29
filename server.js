24f315b09829d11c99385d9ce5f071b678305e1f
Stian <stian.oek@gmail.com>
2022-08-29 21:03:08 +0200

    asdasd

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
   res.json({ name: "json", age: "33" });
 });
 
-const PORT = 4444;
+const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log(`Server will start on http://localhost:` + PORT);
