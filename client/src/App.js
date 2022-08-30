022b9f725415f61c5dd65357e4b7f1eb8e9df8c0
StianOek <66445281+StianOek@users.noreply.github.com>
2022-08-30 12:40:47 +0200

    adding rewrites

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <div>
-      <h1>Fetch from mongoDBasd</h1>
+      {/*  <h1>Fetch from mongoDBasd</h1> */}
       <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<h1>Home</h1>} />
