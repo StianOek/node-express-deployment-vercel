47fa64e5c783fc30402de0b41199bbf5ab1c2e07
StianOek <66445281+StianOek@users.noreply.github.com>
2022-08-30 12:35:31 +0200

    test55

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <div>
       <h1>Fetch from mongoDBasd</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
