f00d2a098f3824224af1c1234c761176d9c8ac4f
Stian <stian.oek@gmail.com>
2022-08-30 09:01:42 +0200

    adding sendFile

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 import express from "express";
+import * as path from "path";
 
 const app = express();
+
 app.use(express.static("./client/index.html"));
+
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    return res.sendFile(path.resolve("./client/index.html"));
+  } else {
+    next();
+  }
+});
 
 app.get("/api/name", (req, res) => {
   res.json({ name: "json", age: "33" });
